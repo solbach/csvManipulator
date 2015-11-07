a287bf0a412ed1a5cf5f37299dffc7198f45b7e4
solbach <mksolb@gmail.com>
2015-11-06 19:23:04 -0500

    some more structure and 'outlier remover' added

--- csvManipulator/src/data/mining/csv/CSV.java
+++ csvManipulator/src/data/mining/csv/CSV.java
@@ -7,65 +7,95 @@ import java.io.IOException;
 
 public class CSV {
 
-  public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-	CSV obj = new CSV();
-	obj.run();
+		CSV obj = new CSV();
+		obj.run();
 
-  }
+	}
 
-  public void run() {
+	public void run() throws IOException {
 
-//	String csvFile = "/home/markus/repositories/datamining/csvManipulator/data/risk_train.csv";
-	String csvFile = "/home/markus/repositories/datamining/csvManipulator/data/risk_test.csv";
-	BufferedReader br = null;
-	String line = "";
-	String cvsSplitBy = ",";
-	String output="";
-	int index = 9;
-	
-	try {
+		boolean useMethod = true;
 
+		// String csvFile =
+		// "/home/markus/repositories/datamining/csvManipulator/data/risk_train.csv";
+		String csvFile = "/home/markus/repositories/csvManipulator/data/risk_train.csv";
+		BufferedReader br = null;
+		String line = "";
+		String cvsSplitBy = ",";
+		String output = "";
+		int index = 40;
+
+		/*
+		 * Since this more a playground to manipulate csv files the structure is
+		 * pretty hard to determine (sorry for that). anyways, easy processing
+		 * is done in the 'else' part. For 'heavier' processing I started (I
+		 * think too late) to define dedicated methods
+		 */
 		br = new BufferedReader(new FileReader(csvFile));
-		while ((line = br.readLine()) != null) {
-
-		        // use comma as separator
-			String[] data = line.split(cvsSplitBy);
+		if (useMethod) {
 			
-			output = data[index];
 			
-			output = output.substring(output.lastIndexOf('.') + 1);
-			//output = output.substring(0, output.length() - 1);
-//			output = output.split("\\:")[0];	
-//			int k = output.indexOf("-");
-//						
-//			if(k != -1)
-//			{
-//				// 2002-12-05
-//				output = output.split("\\-")[0];
-//			}
-//			else{
-//				// 12/28/2003
-//				output = output.substring(output.lastIndexOf('/') + 1);
-//			}
+			detectOutliers(br, index, cvsSplitBy, line, 1000);
+		} else {
 
-			System.out.println("?");
-
-		}
-
-	} catch (FileNotFoundException e) {
-		e.printStackTrace();
-	} catch (IOException e) {
-		e.printStackTrace();
-	} finally {
-		if (br != null) {
 			try {
-				br.close();
+				while ((line = br.readLine()) != null) {
+
+					// use comma as separator
+					String[] data = line.split(cvsSplitBy);
+
+					output = data[index];
+
+					output = output.substring(output.lastIndexOf('.') + 1);
+					output = output.substring(0, output.length() - 1);
+					output = output.split("\\:")[0];
+					int k = output.indexOf("-");
+
+					if (k != -1) {
+						// 2002-12-05
+						output = output.split("\\-")[0];
+					} else {
+						// 12/28/2003
+						output = output.substring(output.lastIndexOf('/') + 1);
+					}
+
+					System.out.println("?");
+
+				}
+
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
 			} catch (IOException e) {
 				e.printStackTrace();
+			} finally {
+				if (br != null) {
+					try {
+						br.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
 			}
 		}
 	}
-  }
+
+	/** outputs the index of a row which is higher than outlierMax */
+	public void detectOutliers(BufferedReader br, int index, String csvSplit, String line, int outlierMax) throws IOException {
+		double output = 0;
+		int counter = 1;
+		System.out.println("Row" + " \t-\t " + "Value");
+		while ((line = br.readLine()) != null) {
+			counter++;
+			String[] data = line.split(csvSplit);
+			output = Double.parseDouble(data[index]);
+			
+			if(output >= 800)
+			{
+				System.out.println(counter + " \t-\t " + output);
+			}
+		}
+	}
 
 }
